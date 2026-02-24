e0f0dfb8533640ecb6355d9560f0c9a5e1773deb
enzo2628aldavis <enzo.aldavis@escola.pr.gov.br>
2026-02-24 09:48:16 -0300

    Add files via upload

---
+++ Media_SE.js
@@ -0,0 +1,14 @@
+let aluno="robson";
+let n1=99;
+let n2=100;
+let n3=99;
+let n4=10;
+let media=0;
+let result="";
+media=(n1+n2+n3+n4)/4;
+if(media>=70){
+    result="aprovado"}
+else{
+    result="reprovado"
+}
+console.log("O aluno " + aluno + "tem a media" + media + "e esta: "+result);
