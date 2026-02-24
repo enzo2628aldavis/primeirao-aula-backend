3a561be2425c9e09e900f9e938622a20c5ba28ae
enzo2628aldavis <enzo.aldavis@escola.pr.gov.br>
2026-02-24 07:53:00 -0300

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,7 @@
+aluno="pedro";
+nota1=5;
+nota2=29;
+nota3=90;
+nota4=10;
+media=(nota1+nota2+nota3+nota4)/4;
+console.log("O aluno" ,aluno,"tem a media", media);
---
+++ teste3.js
@@ -0,0 +1,7 @@
+
+Delta=0;
+b=5;
+a=2;
+c=-5;
+x=b*b*4*a*c;
+console.log("o valor é, delta", x);
